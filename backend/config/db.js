aeddfa42bc6dfa7e012b92078931fa0b8fa5ef69
UjjwalKharkwal <tomailujjwalkharkwal@gmail.com>
2025-09-05 18:48:29 +0530

    Initial commit

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+// import mongoose from "mongoose";
+
+// export const  connectDB = async () =>{
+
+//     await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB Connected"));
+   
+// }
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB Connection Failed:", error);
+    process.exit(1);
+  }
+};
